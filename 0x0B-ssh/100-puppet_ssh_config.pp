833a32c1f6fcfc831935dccf39d2cada6ef0d6bb
Reinhardbett <bettkiptoo46@gmail.com>
2024-08-16 02:56:59 -0700

    feat: use puppet to disable password authentication and specify public key filepath

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,13 @@
 # Puppet manifest used to manage SSH client configurations
 
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('/alx-system_engineering-devops/my_module/ssh_config.erb'),
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
